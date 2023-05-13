48edc22e12771ef1430b3dbde9293653c7193951
billal2022 <billal.tj@outlook.com>
2023-05-13 16:46:33 +0300

    SEO little updates

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,67 +3,20 @@ import "./Header.css";
 import { GoMarkGithub } from "react-icons/go";
 import { BsLinkedin } from "react-icons/bs";
 import { SiFrontendmentor } from "react-icons/si";
-import Animated_arrow from "../../constants/animated_arrow/animated_arrow";
+import AnimatedArrow from "../../constants/animated_arrow/animated_arrow";
 import Button from "../../constants/button/Button";
 import { Cursor, Typewriter } from "react-simple-typewriter";
 
-function Header() {
+const Header = () => {
   return (
     <header id="Home" className="bubbles">
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
+      {Array.from({ length: 50 }).map((_, i) => (
+        <div key={i} className="bubble"></div>
+      ))}
       <section className="app__Header">
         <h1 className="title">
-          HELLO I'M{"   "}
+          HELLO I'M{" "}
           <span>
-            {" "}
             <Typewriter
               words={["BILLAL TADJINE", "FRONTEND DEVELOPER"]}
               loop={Infinity}
@@ -79,37 +32,36 @@ function Header() {
         <p className="app__Header-p">
           Welcome to my website! I am a front-end web developer who applies his
           skills in building websites.
-        </p>{" "}
-        <Button Button="PROJECTS" href={"#Projects"} />
+        </p>
+        <Button Button="PROJECTS" href="#Projects" />
       </section>
       <div className="app__Header-contact">
         <a
-          target={"_blank"}
+          target="_blank"
           href="https://github.com/billal2022"
           title="Github link"
           rel="noreferrer">
           <GoMarkGithub />
         </a>
         <a
-          target={"_blank"}
+          target="_blank"
           href="https://www.linkedin.com/in/billal-tadjine-ba314a24b/"
           title="LinkedIn link"
           rel="noreferrer">
-          {" "}
           <BsLinkedin />
         </a>
         <a
-          target={"_blank"}
+          target="_blank"
           href="https://www.frontendmentor.io/profile/billal2022"
           title="Frontendmentor link"
           rel="noreferrer">
           <SiFrontendmentor />
         </a>
       </div>
-      {/**arrow animated */}
-      <Animated_arrow />
+      {/* animated arrow */}
+      <AnimatedArrow />
     </header>
   );
-}
+};
 
 export default Header;
